6db90de3ea8d4f4b622fa0c47d563bac750e6332
Louskil <tbikulov@gmail.com>
2026-09-06 03:44:43 +0100

    Лабораторна робота 1: Vue у Docker

---
+++ lab1/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>Дані студента</h1>
+    <p><strong>Спеціальність:</strong> Інженерія програмного забезпечення</p>
+    <p><strong>Курс:</strong> 2</p>
+    <p><strong>Група:</strong> 7.F2.25-2</p>
+    <p><strong>ПІБ:</strong> Бікулов Тимур Дамірович</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<style scoped>
+.hello {
+  font-family: Arial, sans-serif;
+  text-align: center;
+}
+</style>
